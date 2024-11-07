285dddc5acadcb3190649f7c03f2529f8b0eb281
Алексей <ix.fenix@mail.ru>
2024-11-07 17:37:11 +0300

    feat: camera movement

---
+++ BadLand_Prototype/Assets/_Source/Camera/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Camera
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private void FixedUpdate()
+        {
+            transform.position += transform.right * (speed * Time.fixedDeltaTime);
+        }
+    }
+}
